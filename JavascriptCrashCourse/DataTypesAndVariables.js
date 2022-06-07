26f33f5603162ecdde58394b6feb3ccf26a2415b
ryancomito <ryancomito.sbc@gmail.com>
2022-06-07 16:49:11 +1000

    intro to data types and variables

---
+++ JavascriptCrashCourse/DataTypesAndVariables.js
@@ -0,0 +1,24 @@
+/* Data types:
+undefined, null, boolean, string, symbol, number, object
+*/
+
+var myName = "Ryan";
+
+myName = 8;
+
+let ourName = "Hello Sir";
+
+const pi = 3.14;
+
+console.log(a); //can check values of vertain variables at different times in code
+// Declare variables:
+var a;
+
+// Assign variables:
+a = 2;
+
+//Declare and assign (initialise) variables:
+var b = 7;
+
+//Seeing things in the consol
+console.log(a);
