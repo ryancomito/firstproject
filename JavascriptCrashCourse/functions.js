f6ab746defc72c7657815a3b3b8b6929a372a1c6
ryancomito <ryancomito.sbc@gmail.com>
2022-06-10 15:50:51 +1000

    switch statements and card counting app

--- JavascriptCrashCourse/functions.js
+++ JavascriptCrashCourse/functions.js
@@ -72,3 +72,20 @@ console.log("After: " + JSON.stringify(testArray));
 function welcomeToBooleans () {
   return true;
 }
+
+  // Returning booleans from a functions
+  function isLess(a, b) {
+    return a < b;
+  }
+  console.log(isLess(5,2));
+
+  // Returning early from a function
+  function abTest(a, b) {
+    if (a < 0 || b < 0) {
+      return undefined;
+    }
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+  }
+  console.log(abTest(16,25));
+
+  
--- JavascriptCrashCourse/switchStatements.js
+++ JavascriptCrashCourse/switchStatements.js
@@ -23,3 +23,48 @@ console.log(caseInSwitch(2));
 
 // Default options
   // Kind of like else in if else statements
+  function switchOfStuff(val) {
+    var answer = "";
+    switch(val) {
+      case "a": 
+        answer = "apple";
+        break;
+      case "b":
+        answer = "bird";
+        break;
+      case "c":
+        answer = "cat";
+        break;
+      default: // default answer if none of the previous cases are true
+        answer = "stuff";
+        break;
+    }
+    return answer;
+  }
+  console.log(switchOfStuff(a));
+
+  // Multiple cases having the same consequence
+    // Omit the break statement and code until the final case
+  function sequentialSizes(val) {
+    var answer = "";
+    switch (val) {
+      case 1:
+      case 2:
+      case 3:
+        answer = "Low";
+        break;
+      case 4:
+      case 5:
+      case 6:
+        answer = "Mid";
+        break;
+      case 7:
+      case 8:
+      case 9:
+        answer = "High";
+        break;
+    }
+    return answer;
+  }
+  console.log(sequentialSizes(2));
+
