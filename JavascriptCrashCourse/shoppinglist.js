809a7175ff280ea4307fea4f8ebbdbff2ceb3820
ryancomito <ryancomito.sbc@gmail.com>
2022-06-12 19:32:48 +1000

    beggining shopping list site

--- JavascriptCrashCourse/shoppinglist.js
+++
@@ -1,4 +0,0 @@
-// Shopping list with nested arrays
-  // Make this a simple js website?
-
-var shoppingList = [["cereal, 3"], ["milk", 1], ["apples", 4], ["eggs", 1]];
---
+++ JavascriptCrashCourse/shoppinglist/shoppinglist.js
@@ -0,0 +1,5 @@
+// Shopping list with nested arrays
+  // Make this a simple js website?
+
+var shoppingList = [["cereal", 3], ["milk", 1], ["apples", 4], ["eggs", 1]];
+var shoppingItems = document.querySelector(".row");
